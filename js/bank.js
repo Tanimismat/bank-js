6a41c8c39c1b768f465866097ca5a465c6ee42a4
Tanimismat <tanim.ismat@gmail.com>
2022-02-13 21:20:27 +0600

    first commit

--- js/bank.js
+++ js/bank.js
@@ -1,27 +1,30 @@
 // add deposite event handeler
 document.getElementById('deposite-button').addEventListener('click', function () {
     // console.log('deposite button clicked')
-    // get the amount deposited
+    // 1. get the amount deposited
     const depositeInput = document.getElementById('deposite-input');
     const newdepositeAmountText = depositeInput.value;
+    // console.log(newdepositeAmountText);
     const newdepositeAmount = parseFloat(newdepositeAmountText)
-    // console.log(depositeAmount);
+    // console.log(newdepositeAmount);
 
-    //update deposite total
+
+
+    // 2.update deposite total
     const depositeTotal = document.getElementById('deposite-total');
     const previousDepositetext = depositeTotal.innerText;
     const previousDepositeAmount = parseFloat(previousDepositetext);
     const newDepositeTotal = previousDepositeAmount + newdepositeAmount;
     depositeTotal.innerText = newDepositeTotal;
 
-    //update account balance
+    // 3.update account balance
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceAmount = balanceTotal.innerText;
     const newBalanceTotal = parseFloat(newdepositeAmount) + parseFloat(previousBalanceAmount);
     balanceTotal.innerText = newBalanceTotal;
 
 
-    // clear the deposite input field
+    // 4.clear the deposite input field
     depositeInput.value = ' ';
 })
 
@@ -43,6 +46,7 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
 
     //clear withdraw input
     withdrawInput.value = ' ';
+    // ekhane ekta space dite hobe
 
     //update balance
     const balanceAfterWithdraw = document.getElementById('balance-total');
@@ -51,8 +55,4 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 
     balanceAfterWithdraw.innerText = newBalanceTotal;
-
-
-
-    // ekhane ekta space dite hobe
 })
